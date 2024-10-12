c6abb294caa23cf68572449c6b2754960bdd6314
Van <MisterDeterminator@yandex.ru>
2024-10-12 12:23:08 +0000

    made home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,36 @@
-import { Button } from '../components/common/button/button'
-import { TextInput } from '../components/common/textInput/textInput'
+import { StyledButton } from '../components/common/button/button'
 import { DropDownMenu } from '../components/common/dropDownMenu/dropDownMenu'
+import { FlexBox } from '../components/common/flexBox/flexBox'
+import { ThemeSwitcher } from '../components/themeSwitcher/themeSwitcher'
+
+import { Card } from '../components/card'
 
 import styled from 'styled-components'
 
 import "../styles/font_import.css"
-import { font } from '../styles/constants'
+import { colors } from '../styles/constants'
 
 export default function Home() {
     return (
-        <StyledHome>
-            <Button text="Новый вклад" />
-            <TextInput placeholder="Сумма" />
-            <DropDownMenu options={["дн", "мес", "год"]}
-                          accent={false}
-                          defaultValue="Выберите..."
-            />
-        </StyledHome>
+        <>
+            <Header>
+                <FlexBox $justifyContent='space-between'>
+                    <StyledButton>Новый вклад</StyledButton>
+                    <FlexBox $justifyContent='space-between'>
+                        <DropDownMenu options={["рус", "eng"]} defaultValue='рус'/>
+                        <ThemeSwitcher></ThemeSwitcher>
+                    </FlexBox>
+                </FlexBox>
+            </Header>
+            <Card></Card>
+        </>
     )
 }
 
-const StyledHome = styled.section`
-    font-family: ${font.family};
-`;
+const Header = styled.header`
+    padding: 48px 55px;
+    filter: drop-shadow(0 0 10px grey);
+    background-color: ${colors.light};
+`
 
   
