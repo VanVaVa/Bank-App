f80e8f33a1aff68d610acbb819e86e33c25c8a57
Van <MisterDeterminator@yandex.ru>
2024-10-12 12:24:16 +0000

    button fixed: deleted wrap

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -1,20 +1,8 @@
-import React from 'react';
 import styled from 'styled-components'
 
 import { colors, font } from '../../../styles/constants'
 
-
-type ButtonProps = {
-  text: string;
-};
-
-export const Button: React.FC<ButtonProps> = ({ text }) => {
-    return (
-        <StyledButton>{text}</StyledButton>
-    )
-}
-
-const StyledButton = styled.button`
+export const StyledButton = styled.button`
     background-color: ${colors.accent};
     color: white;
     font-size: ${font.size};
